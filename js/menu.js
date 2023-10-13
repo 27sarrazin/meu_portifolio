91a1f59379e0e14fc71cd6ce50b147b4c0b6b205
27sarrazin <sarrazinr5@gmail.com>
2023-10-13 18:48:11 -0400

    primeiro commit

---
+++ js/menu.js
@@ -0,0 +1,7 @@
+function  menu(){
+    let menu = document.getElementById("menu-mobile")
+    if (menu.style.display == "none")
+        menu.style.display = "block"
+    else
+        menu.style.display = "none"
+}
